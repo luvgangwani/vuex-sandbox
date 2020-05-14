fe3c26aeb4bcfb3e99f82f07c5b92bdde06d1dab
Luv Gangwani <luvgangwani>
2020-05-14 21:33:40 +1000

    Lesson 2 - A Simple Vue App

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="product-list-one">
+        <h2>Product List One</h2>
+        <ul>
+            <li v-bind:key="index" v-for="(product, index) in products">
+                <p>{{ product.name }}</p>
+                <p>${{ product.price }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: ['products']
+}
+</script>
+
+<style scoped>
+    
+    #product-list-one {
+        width: 800px;
+        background-color: #6dfa6da8;
+        padding: 25px;
+        display: inline-block;
+        border-radius: 10px;
+    }
+
+    #product-list-one ul {
+        list-style: none;
+        padding: 0px;
+    }
+
+    #product-list-one ul li {
+        background-color: #ffffff;
+        padding: 5px;
+        margin: 10px 0px;
+        border-radius: 10px;
+    }
+
+
+</style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="product-list-two">
+        <h2>Product List Two</h2>
+        <ul>
+            <li v-bind:key="index" v-for="(product, index) in products">
+                <p>{{ product.name }}</p>
+                <p>${{ product.price }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: ['products']
+}
+</script>
+
+<style scoped>
+
+#product-list-two {
+    width: 800px;
+    background-color: #6dc2faa8;
+    padding: 25px;
+    margin-top: 20px;
+    display: inline-block;
+    border-radius: 10px;
+}
+
+#product-list-two ul {
+    list-style: none;
+    padding: 0px;
+}
+
+#product-list-two ul li {
+    background-color: #ffffff;
+    padding: 5px;
+    margin: 10px 0px;
+    border-radius: 10px;
+}
+    
+</style>
